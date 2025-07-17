12f612e91979ce54de0ecd97b302f38bb318b3f1
the3dit0r <76208541+The3dit0r@users.noreply.github.com>
2025-07-17 07:58:10 +0700

    [bug] Fix loading screen stuck

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./index.css";
 
 import { useServices } from "@services/index";
@@ -6,9 +7,22 @@ import { AuthenticationPages, AuthenticatedPage } from "@pages";
 import FullscreenLoading from "@pages/others/loading";
 
 export default function ApplicationLayout() {
-  const { auth } = useServices();
+  const { auth, client } = useServices();
+  const [loading, setLoading] = useState(true);
 
-  if (!auth.userDetails) {
+  function handleAuthChange() {
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    const s = client.auth.onAuthStateChange(handleAuthChange);
+
+    return () => {
+      s.data.subscription.unsubscribe();
+    };
+  }, [client]);
+
+  if (loading) {
     return <FullscreenLoading showLogo showPlaceholder />;
   }
 
